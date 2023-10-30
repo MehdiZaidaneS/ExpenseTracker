827c8a97018b8646a36ea03866e696ef302990e7
Mehdi <mehdicontacto@gmail.com>
2023-10-30 17:19:31 +0200

    Working on front end

---
+++ frontend/src/Components/Incomes/Income.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import { InnerLayout } from '../../styles/Layout';
+
+function Income(props) {
+    return (
+        <IncomeStyled>
+            <InnerLayout>
+                
+            </InnerLayout> 
+        </IncomeStyled>
+    );
+}
+
+
+const IncomeStyled = styled.div`
+
+`;
+
+export default Income;
